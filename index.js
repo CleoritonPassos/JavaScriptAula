14ed5aa520d45b807632c63e3f5abb661dfb6dab
CleoritonPassos <cleoritonpassos@hotmail.com>
2021-12-15 21:03:13 -0300

    Primeira Aula

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function carregamentoPagina() {
-    alert("Menssagem apresentada no carregamento do sistema");
-}
+// function carregamentoPagina() {
+//     alert("Menssagem apresentada no carregamento do sistema");
+// }
+
+$(() => {
+    // codigo executado no carregamento da pagina
+    // alert("Menssagem apresentada no carregamento do sistema");
+
+    $("#btnSalvar").click(() => {
+        console.log("FUNÇAO CLICK FUNCIONOU")
+    })
+
+})
